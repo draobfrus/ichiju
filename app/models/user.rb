accd95f36b7a57a764769990e2e92e0aaaf077d1
draobfrus <aelfci5am@gmail.com>
2022-12-30 20:24:50 +0900

    [Add]user.rbにバリデーション、enum role, JpPrefecture導入設定を追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  enum role: { general: 0, admin: 1 }
+
+  include JpPrefecture
+  jp_prefecture :birthplace_code, method_name: :birthplace
+  jp_prefecture :living_place_code, method_name: :living_place
 end
