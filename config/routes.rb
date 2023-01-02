1252239dbefa5f5779bcf72e72483c0e70dde1c0
draobfrus <aelfci5am@gmail.com>
2023-01-02 21:03:14 +0900

    [Add]ログイン機能に係るrouting, view, controller生成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "static_pages#top"
+  root 'static_pages#top'
 
   resources :users, only: %i[new create]
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
