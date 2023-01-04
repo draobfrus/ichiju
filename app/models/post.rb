124898f5f4701cb3b288e9dd04579aa37bab3d02
draobfrus <aelfci5am@gmail.com>
2023-01-04 16:57:48 +0900

    [Add]ImageUploaderを作成し、Postモデルに紐付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,6 +21,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  mount_uploader :image, ImageUploader
+
   validates :title, presence: true, length: { maximum: 255 }
   validates :image, presence: true
   validates :content, length: { maximum: 65535 }
