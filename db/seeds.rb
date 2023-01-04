021db7e9a8e0af790f778c8fabf33e95bdbe5d62
draobfrus <aelfci5am@gmail.com>
2023-01-04 17:53:34 +0900

    [Add]User, Postのダミーオブジェクトを10個作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do |n|
+  User.create!(
+    username: "テストユーザー#{n+1}",
+    email: "test#{n+1}@example.com",
+    password: "password",
+    password_confirmation: "password"
+  )
+end
+
+User.all.each do |user|
+  Post.create!(
+    user_id: user.id,
+    title: "タイトル#{user.id}",
+    content: "本文#{user.id}",
+    image: File.open("./app/assets/images/misosoup.png")
+  )
+end
