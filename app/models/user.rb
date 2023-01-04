be886cd4c32da587eb17d22345b4f80f9b13f760
draobfrus <aelfci5am@gmail.com>
2023-01-04 16:19:46 +0900

    [Add]各モデルファイルにアソシエーション・バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :posts
+
   validates :username, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
