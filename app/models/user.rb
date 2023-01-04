1a275b8f0a9baa93a91fd52323e4bcd30be877e2
draobfrus <aelfci5am@gmail.com>
2023-01-04 16:23:40 +0900

    [Add]bundle exec rails g annotate:install実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :bigint           not null, primary key
+#  avatar            :string
+#  birthplace_code   :integer
+#  crypted_password  :string
+#  email             :string           not null
+#  living_place_code :integer
+#  role              :integer          default("general"), not null
+#  salt              :string
+#  username          :string(255)      not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
