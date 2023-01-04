903e858104431915686632b88619d7fcf28bfd25
draobfrus <aelfci5am@gmail.com>
2023-01-04 20:16:57 +0900

    [Add]投稿機能に係るrouting, coontroller, viewを設置

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
 
+  resources :posts
+
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
