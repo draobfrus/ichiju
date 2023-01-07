060bfd398d808efc0bcd151bba939790480a7c36
draobfrus <aelfci5am@gmail.com>
2023-01-07 14:28:50 +0900

    [Add]投稿作成機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,18 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to posts_url, success: t('messages.success', word: Post.model_name.human)
+    else
+      flash.now[:danger] = t('messages.danger', word: Post.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -21,5 +30,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :image, :image_cache)
   end
 end
