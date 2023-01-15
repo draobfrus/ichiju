5c34121c7cb656735036db480a9ea69aea6fb60b
draobfrus <aelfci5am@gmail.com>
2023-01-15 23:29:51 +0900

    [Add]Ingredientモデルにアソシエーションとバリデーションを追加

--- spec/factories/ingredients.rb
+++ spec/factories/ingredients.rb
@@ -8,6 +8,10 @@
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
 #
+# Indexes
+#
+#  index_ingredients_on_name  (name) UNIQUE
+#
 FactoryBot.define do
   factory :ingredient do
     name { "MyString" }
