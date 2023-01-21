769474c2bb77fcc09ae1acede69d7ad69eabdadf
draobfrus <aelfci5am@gmail.com>
2023-01-21 19:24:28 +0900

    [Add]楽天APIの商品を検索し、結果を表示する機能を追加

--- app/controllers/misosoup_bases_controller.rb
+++ app/controllers/misosoup_bases_controller.rb
@@ -1,4 +1,9 @@
 class MisosoupBasesController < ApplicationController
   def new
+    # params[:keyword]に合致したデータをresultsに格納する
+    if params[:keyword].present?
+      @results = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+    end
   end
+
 end
