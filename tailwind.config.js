a2759e5efad950bd769bba64707248386ee0ce96
draobfrus <aelfci5am@gmail.com>
2023-02-11 18:14:42 +0900

    [Add]トップページ改善

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'sumire': '#d1d0ee',
+        'botan': '#ef5241',
+        'azuki': '#6a3f4c',
+        'tamago': '#fff5ac',
+        'sakura': '#ffa67e',
+        'ohagi': '#ece3e6',
+      },
+    },
+  },
   plugins: [
     require('daisyui'),
   ],
