eff3666c2313c36978dcb5c7ff4ec00b10849f45
draobfrus <aelfci5am@gmail.com>
2023-02-13 18:25:57 +0900

    [Add]production.user.rbを再度変更

--- db/fixtures/production/01_user.rb
+++ db/fixtures/production/01_user.rb
@@ -2,8 +2,8 @@ User.create!(
   username: "みそ汁おねいさん",
   birthplace_code: 4,
   living_place_code: 7,
-  email: <%= ENV["ADMIN_EMAIL"] %>,
-  password: <%= ENV["ADMIN_PASSWORD"] %>,
-  password_confirmation:  <%= ENV["ADMIN_PASSWORD"] %>,
-  role: "admin",
+  email: ENV["ADMIN_EMAIL"],
+  password: ENV["ADMIN_PASSWORD"],
+  password_confirmation: ENV["ADMIN_PASSWORD"],
+  role: "admin"
 )
