bf7cb852da20c657926fc50470171c7adfdfe1e0
draobfrus <aelfci5am@gmail.com>
2023-02-13 18:17:38 +0900

    [Add]fixtures/productionの中身を変更

--- db/fixtures/production/01_user.rb
+++ db/fixtures/production/01_user.rb
@@ -2,8 +2,8 @@ User.create!(
   username: "みそ汁おねいさん",
   birthplace_code: 4,
   living_place_code: 7,
-  email: "aelfci5am@yahoo.co.jp",
+  email: <%= ENV["ADMIN_EMAIL"] %>,
+  password: <%= ENV["ADMIN_PASSWORD"] %>,
+  password_confirmation:  <%= ENV["ADMIN_PASSWORD"] %>,
   role: "admin",
-  password: "password",
-  password_confirmation: "password"
 )
