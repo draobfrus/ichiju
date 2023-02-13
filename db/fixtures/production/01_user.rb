979300eff4b6d511f245144b30fefeac7384949e
draobfrus <aelfci5am@gmail.com>
2023-02-13 13:56:41 +0900

    [Add]データベース変更、entrypoint.sh追記

---
+++ db/fixtures/production/01_user.rb
@@ -0,0 +1,9 @@
+User.create!(
+  username: "みそ汁おねいさん",
+  birthplace_code: 4,
+  living_place_code: 7,
+  email: "aelfci5am@yahoo.co.jp",
+  role: "admin",
+  password: "password",
+  password_confirmation: "password"
+)
