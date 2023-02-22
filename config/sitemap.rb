1dd6e51b66ae4683a00c1e34caf3e77a15d11592
draobfrus <aelfci5am@gmail.com>
2023-02-22 22:10:16 +0900

    [Add]sitemap.rbに設定を追加

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,12 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.ichiju.jp"
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}"
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
+  ENV['S3_BUCKET_NAME'],
+  aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  aws_region: ENV['AWS_DEFAULT_REGION'],
+)
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
