9ef4b5251ea4c91a1cc4170672aed023bdd9d5a8
draobfrus <aelfci5am@gmail.com>
2023-02-25 14:32:06 +0900

    [Add]プロフィール機能に係るコントローラ設定

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,26 @@
 class ProfilesController < ApplicationController
-  def show
-  end
+  before_action :set_user, only: %i[edit update]
 
-  def edit
-  end
+  def show; end
+
+  def edit; end
 
   def update
+    if @user.update(user_params)
+      redirect_to profile_url(@user), success: t('defaults.message.success', word: t('defaults.update'))
+    else
+      flash.now[:danger] = t('defaults.message.danger', word: t('defaults.update'))
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :birthplace_code, :living_place_code, :avatar, :avatar_cache)
   end
 end
