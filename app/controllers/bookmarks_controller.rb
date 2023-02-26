f29db74d72f88603f25f6531e0184e7b18b0b9c7
draobfrus <aelfci5am@gmail.com>
2023-02-26 15:03:19 +0900

    [Add]投稿一覧画面にブックマークボタンを追加

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,12 +2,12 @@ class BookmarksController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     current_user.bookmark(post)
-    redirect_back fallback_location: post_path(post), success: 'ブックマークしました'
+    redirect_back fallback_location: posts_path, success: 'ブックマークしました'
   end
 
   def destroy
     post = current_user.bookmarks.find(params[:id]).post
     current_user.unbookmark(post)
-    redirect_back fallback_location: post_path(post), success: 'ブックマークを解除しました', status: :see_other
+    redirect_back fallback_location: posts_path, success: 'ブックマークを解除しました', status: :see_other
   end
 end
