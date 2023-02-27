b26f2f542f857a852c55b866d89b606f1ec03c40
draobfrus <aelfci5am@gmail.com>
2023-02-27 16:26:45 +0900

    [Add]application_helperの記述を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,28 +3,28 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: t('meta_tags.defaults.site'),
+      title: t('meta_tags.defaults.title'),
+      reverse: true,
+      separator: '|',
       description: t('meta_tags.defaults.description'),
       keywords: t('meta_tags.defaults.keywords'),
-      separator: '|',
-      reverse: true,
+      charset: "UTF-8",
       canonical: request.original_url,
       noindex: ! Rails.env.production?,
-      charset: "UTF-8",
       icon: image_url('favicon.png'),
       og: {
+        site_name: t('meta_tags.defaults.site'),
+        title: t('meta_tags.defaults.title'),
+        description: t('meta_tags.defaults.description'),
         type: 'website',
-        title: :title,
-        description: :description,
         url: request.original_url,
         image: image_url('image.png'),
-        site_name: :site,
         local: 'ja-JP'
       },
       twitter: {
+        card: 'summary_large_image',
         site: '@draobfrus',
-        card: 'summary',
-        image: image_url('image.png'),
-      }
+      },
     }
   end
 end
