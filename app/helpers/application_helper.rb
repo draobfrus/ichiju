753a844ad8e5c6ce1f881f4d710c2d9da9e35bb0
draobfrus <aelfci5am@gmail.com>
2023-02-27 14:51:34 +0900

    [Add]デフォルトのメタタグとOGPを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,46 @@ module ApplicationHelper
   def page_title(page_title = '')
     page_title.empty? ? '一汁' : "#{page_title} | 一汁"
   end
+
+  def show_meta_tags
+    if display_meta_tags.blank?
+      assign_meta_tags
+    end
+    display_meta_tags
+  end
+
+  def assign_meta_tags(options = {})
+    defaults = t('meta_tags.defaults')
+    options.reverse_merge!(defaults)
+
+    site = options[:site]
+    title = options[:title]
+    description = options[:description]
+    keywords = options[:keywords]
+    image = options[:image].presence || image_url('image.png')
+
+    configs = {
+      separator: '|',
+      reverse: true,
+      site: site,
+      title: title,
+      description: description,
+      keywords: keywords,
+      canonical: request.original_url,
+      og: {
+        type: 'article',
+        title: title.presence || site,
+        description: description,
+        url: request.original_url,
+        image: image,
+        site_name: site
+      },
+      twitter: {
+        site: '@draobfrus',
+        card: 'summary',
+      }
+    }
+
+    set_meta_tags(configs)
+  end
 end
