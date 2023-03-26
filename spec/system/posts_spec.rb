88ac5490a018de91cdf76a95e31d86a26e2f9e28
draobfrus <aelfci5am@gmail.com>
2023-03-26 20:13:24 +0900

    [Add]投稿機能に係るシステムスペック（ログイン後）を作成

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :system do
-  let(:user) { create(:user)}
-  let(:post) { create(:post)}
+  let(:post) { create(:post) }
 
   describe 'ログイン前' do
     describe 'ページ遷移確認' do
@@ -39,6 +38,92 @@ RSpec.describe 'Posts', type: :system do
           expect(page).to have_content post_list[1].title
           expect(page).to have_content post_list[2].title
           expect(current_path).to eq posts_path
+        end
+      end
+    end
+  end
+
+  describe 'ログイン後' do
+    let(:user) { create(:user) }
+    before { login_as(user) }
+
+    describe '投稿作成' do
+      context 'フォームの入力値が正常' do
+        it '投稿の新規作成が成功する' do
+          visit new_post_path
+          fill_in '*タイトル', with: 'title'
+          attach_file '*写真', "#{Rails.root}/spec/fixtures/test_image.png"
+          click_button '投稿する'
+          expect(page).to have_content ('投稿に成功しました')
+          expect(current_path).to eq posts_path
+        end
+      end
+
+      context 'タイトルが未入力' do
+        it '投稿の新規作成が失敗する' do
+          visit new_post_path
+          fill_in '*タイトル', with: ''
+          attach_file '*写真', "#{Rails.root}/spec/fixtures/test_image.png"
+          click_button '投稿する'
+          expect(page).to have_content ('投稿できませんでした')
+          expect(page).to have_content ('タイトルを入力してください')
+        end
+      end
+
+      context '画像が未入力' do
+        it '投稿の新規作成が失敗する' do
+          visit new_post_path
+          fill_in '*タイトル', with: 'title'
+          click_button '投稿する'
+          expect(page).to have_content ('投稿できませんでした')
+          expect(page).to have_content ('写真を入力してください')
+        end
+      end
+    end
+
+    describe '投稿編集' do
+      let!(:post) { create(:post, user: user) }
+
+      context 'フォームの入力値が正常' do
+        it '投稿の編集が成功する' do
+          visit edit_post_path(post)
+          fill_in '*タイトル', with: 'new_title'
+          click_button '投稿する'
+          expect(page).to have_content ('更新に成功しました')
+        end
+      end
+
+      context 'タイトルが空欄' do
+        it '投稿の編集が失敗する' do
+          visit edit_post_path(post)
+          fill_in '*タイトル', with: ''
+          click_button '投稿する'
+          expect(page).to have_content ('更新できませんでした')
+          expect(page).to have_content ('タイトルを入力してください')
+        end
+      end
+    end
+
+    describe '投稿削除', js: true do
+      let!(:post) { create(:post, user: user) }
+
+      it '投稿の削除が成功する' do
+        visit post_path(post)
+        click_link '削除'
+        expect(page.accept_confirm).to eq '削除しますか？'
+        expect(page).to have_content ('削除に成功しました')
+      end
+    end
+
+    describe 'ページ遷移' do
+      let!(:other_user) { create(:user, email: 'other@example.com') }
+      let!(:other_post) { create(:post, user: other_user) }
+
+      context '他ユーザーの投稿詳細ページへアクセス' do
+        it '編集・削除ボタンが表示されない' do
+          visit post_path(other_post)
+          expect(page).not_to have_content ('編集')
+          expect(page).not_to have_content ('削除')
         end
       end
     end
