165fcf141ea1fe335c4182f2fb85ace0a0f186c1
draobfrus <aelfci5am@gmail.com>
2023-04-25 10:19:15 +0900

    [Add]アフェリエイトIDの設定を追加

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -3,7 +3,7 @@ RakutenWebService.configure do |c|
   c.application_id = ENV.fetch('RWS_APPLICATION_ID', nil)
 
   # (任意) 楽天アフィリエイトID
-  # c.affiliate_id = 'YOUR_AFFILIATE_ID' # default: nil
+  c.affiliate_id = 'YOUR_AFFILIATE_ID' # default: nil
 
   # (任意) リクエストのリトライ回数
   # 一定期間の間のリクエスト数が制限を超えた時、APIはリクエスト過多のエラーを返す。
